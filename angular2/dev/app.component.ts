b64db570cdd534ba4c1991de00d3ea68426b36b8
Arthurisme <is@live.ca>
2016-04-03 21:05:28 -0400

    201604032112

---
+++ angular2/dev/app.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '../node_modules/angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: `
+        <h1>Angular 2 Boilerplate</h1>
+        <p>Hello World!!</p>
+    `,
+})
+export class AppComponent {
+
+}
